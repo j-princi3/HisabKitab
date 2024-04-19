2d60091e142d7a6e9ee2ad7bc11be44b1d9c8553
j-princi3 <jainprinci00@gmail.com>
2024-04-19 17:47:06 +0530

    solved bug related to expense being added multiple times

--- backend_hisab/hisab/views.py
+++ backend_hisab/hisab/views.py
@@ -78,6 +78,8 @@ class TodaysExpense(APIView):
                 date = serializer.validated_data.get('time')
                 no_of_expense = serializer.validated_data.get('no_of_expense')
                 list_of_expense = serializer.validated_data.get('list_of_expense')
+                if Count.objects.filter(time=date).exists():
+                    return Response({"message": "expense for this day already exists"}, status=status.HTTP_404_NOT_FOUND)
                 if no_of_expense == 0:
                     return Response({"message": "No expenses to add"}, status=status.HTTP_201_CREATED)
                 parent = Parent_Expense(shop_name=member.shop_name,time=date,no_of_expense=no_of_expense)
