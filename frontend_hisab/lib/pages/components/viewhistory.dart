13e2a815d867970774ae84e1dfd7de0cf7f3f2c0
j-princi3 <jainprinci00@gmail.com>
2024-04-27 08:35:01 +0530

    search with date

--- frontend_hisab/lib/pages/components/viewhistory.dart
+++ frontend_hisab/lib/pages/components/viewhistory.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:intl/intl.dart';
 import 'package:table_calendar/table_calendar.dart';
 import 'package:frontend_hisab/services/searchwithdate_api.dart';
 
@@ -16,35 +16,7 @@ class _TableBasicsExampleState extends State<TableBasicsExample> {
   DateTime _focusedDay = DateTime.now();
   DateTime _selectedDay = DateTime.now(); // Initialize _selectedDay to DateTime.now()
 
-  @override
-  void initState() {
-    super.initState();
-    _loadSelectedDate();
-  }
-
-void _loadSelectedDate() async {
-  SharedPreferences prefs = await SharedPreferences.getInstance();
-  String? selectedDate = prefs.getString('selected_date');
-  if (selectedDate != null) {
-    setState(() {
-      _selectedDay = DateTime.parse(selectedDate);
-    });
-    final response = await APIService.getDate(_selectedDay);
-    if (response['success']) {
-      // If the API call is successful, update the UI with the response data
-      print(response['data']);
-    } else {
-      // If the API call is unsuccessful, show an error message
-      print(response['error']);
-    }
-  }
-}
-
-  void _saveSelectedDate(DateTime date) async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    // Save the selected date to shared preferences
-    await prefs.setString('selected_date', date.toString());
-  }
+ 
 
   @override
   Widget build(BuildContext context) {
@@ -56,26 +28,36 @@ void _loadSelectedDate() async {
       selectedDayPredicate: (day) {
         return isSameDay(_selectedDay, day);
       },
-      onDaySelected: (selectedDay, focusedDay) {
+      onDaySelected: (selectedDay, focusedDay) async {
         if (!isSameDay(_selectedDay, selectedDay)) {
+          print(selectedDay);
+          String formattedDateTime = DateFormat('yyyy-MM-dd').format(selectedDay) ;
+              final response =  APIService.getDate(formattedDateTime);
+    final responseData = await response;
+    if (responseData['success']) {
+      // If the API call is successful, update the UI with the response data
+      // Text('Data: ${responseData['data']}');
+      print(responseData['data']);
+    } else {
+      // If the API call is unsuccessful, show an error message
+      print(responseData['error']);
+    }
           setState(() {
             _selectedDay = selectedDay;
             _focusedDay = focusedDay;
           });
-          // Save the selected date when it changes
-          _saveSelectedDate(selectedDay);
         }
       },
-      onFormatChanged: (format) {
-        if (_calendarFormat != format) {
-          setState(() {
-            _calendarFormat = format;
-          });
-        }
-      },
-      onPageChanged: (focusedDay) {
-        _focusedDay = focusedDay;
-      },
+      // onFormatChanged: (format) {
+      //   if (_calendarFormat != format) {
+      //     setState(() {
+      //       _calendarFormat = format;
+      //     });
+      //   }
+      // },
+      // onPageChanged: (focusedDay) {
+      //   _focusedDay = focusedDay;
+      // },
     );
   }
 }
--- frontend_hisab/lib/services/searchwithdate_api.dart
+++ frontend_hisab/lib/services/searchwithdate_api.dart
@@ -1,21 +1,15 @@
 import 'package:http/http.dart' as http;
-import 'package:intl/intl.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 class APIService {
   static const String baseURL = 'http://192.168.0.106:8000'; // Update with your base URL
 
-  static Future<Map<String, dynamic>> getDate(DateTime selectedDay) async  {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    final selectedDate = prefs.getString('dateTime') ?? '';
-    String currentDate = DateFormat('yyyy-MM-dd').format(selectedDate as DateTime);
+  static Future<Map<String, dynamic>> getDate(String selectedDay) async  {
 
     // Make the API call with the current date
-    final response = await http.get(Uri.parse('$baseURL/date/?date=$currentDate'));
+    final response = await http.get(Uri.parse('$baseURL/date/?date=$selectedDay'));
 
     if (response.statusCode == 200) {
 
       // If the server returns a 200 OK response, parse the JSON response
-      print(response.body);
       return {'success': true, 'data': response.body};
     } else {
       // If the server returns an unsuccessful response code, return an error message
